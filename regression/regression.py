b858843b4b2b8094d3ebd6eb5b68a4fd2a72f606
jmacek <kuba.macek@o2.pl>
2019-09-25 20:37:36 +0200

    use quandl api, regression training

--- regression/regression.py
+++ regression/regression.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import quandl
 import math
+import os
+import numpy as np
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
+
+quandl.ApiConfig.api_key = os.environ['QUANDL_API_KEY']
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -13,11 +19,20 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forecast_column = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.01*len(df))) 
-#forecast_out = math.ceil(0) 
+forecast_out = int(math.ceil(0.01*len(df)))
+print("forecast out:{}".format(forecast_out))
 
 df['label'] = df[forecast_column].shift(-forecast_out)
 df.dropna(inplace=True)
 
-print(df.head())
-print(df.tail())
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, Y_train)
+accuracy = clf.score(X_test, Y_test)
+print("determination R^2: {}".format(accuracy))
